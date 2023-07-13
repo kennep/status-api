df66296d977defbae8901959e8ff81a7a2aae6d7
Kenneth Wang Pedersen <kenneth@wangpedersen.com>
2023-07-13 14:32:49 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use std::io;
-use std::collections::HashMap;
 use async_process::Command;
+use std::collections::HashMap;
+use std::io;
 
 use rocket::http::Status;
 use rocket::serde::Deserialize;
-use rocket::{State, fairing::AdHoc};
+use rocket::{fairing::AdHoc, State};
 
 use rocket_governor::{Method, Quota, RocketGovernable, RocketGovernor};
 
-use log::{info,warn};
+use log::{info, warn};
 
 pub struct RateLimitGuard;
 
@@ -24,22 +25,26 @@ impl<'r> RocketGovernable<'r> for RateLimitGuard {
 #[serde(crate = "rocket::serde")]
 struct Probe {
     command: String,
-    args: Option<Vec<String>>
+    args: Option<Vec<String>>,
 }
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Config {
-    probes: HashMap<String, Probe>
+    probes: HashMap<String, Probe>,
 }
 
 #[get("/probes/<name>")]
-async fn get_probe(name: String, config: &State<Config>, _limit: RocketGovernor<'_, RateLimitGuard>) -> (Status, &'static str) {
+async fn get_probe(
+    name: String,
+    config: &State<Config>,
+    _limit: RocketGovernor<'_, RateLimitGuard>,
+) -> (Status, &'static str) {
     let maybe_probe = find_probe(name, config);
     if let Some(probe) = maybe_probe {
         match run_command(probe).await {
             Ok(_) => (Status::Ok, "Ok\n"),
-            Err(_) => (Status::ServiceUnavailable, "Failed\n")
+            Err(_) => (Status::ServiceUnavailable, "Failed\n"),
         }
     } else {
         (Status::NotFound, "Not found\n")
@@ -47,36 +52,44 @@ async fn get_probe(name: String, config: &State<Config>, _limit: RocketGovernor<
 }
 
 #[head("/probes/<name>")]
-async fn head_probe(name: String, config: &State<Config>, limit: RocketGovernor<'_, RateLimitGuard>) -> Status {
+async fn head_probe(
+    name: String,
+    config: &State<Config>,
+    limit: RocketGovernor<'_, RateLimitGuard>,
+) -> Status {
     get_probe(name, config, limit).await.0
 }
 
 fn find_probe(name: String, config: &State<Config>) -> Option<&Probe> {
-    config.probes.iter().find(|(pname, _)| **pname == name).map(|(_, p)| p)
+    config
+        .probes
+        .iter()
+        .find(|(pname, _)| **pname == name)
+        .map(|(_, p)| p)
 }
 
 async fn run_command(probe: &Probe) -> io::Result<()> {
     let args = probe.args.to_owned().unwrap_or(Vec::new());
 
     info!("Running command: {} {:?}", probe.command, args);
-    let output_result = Command::new(&probe.command)
-            .args(args)
-            .output()
-            .await;
+    let output_result = Command::new(&probe.command).args(args).output().await;
     if let Err(command_err) = output_result {
         warn!("Could not execute command: {:?}", command_err);
-        return Err(command_err)
+        return Err(command_err);
     }
 
     let output = output_result.unwrap();
     info!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
-    info!("Stdout: {}", String::from_utf8_lossy(&output.stdout));    
+    info!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
     if output.status.success() {
         info!("Command completed successfully");
         Ok(())
     } else {
         warn!("Command exited with code {}", output.status);
-        Err(io::Error::new(io::ErrorKind::Other, format!("exited with code {}", output.status)))
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("exited with code {}", output.status),
+        ))
     }
 }
 
